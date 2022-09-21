37563293bcf89fe7df296283796655a246b0eb40
matheus.barragao <matheus.barragao@ABRACAM020.ABRACAM.LOCAL>
2022-09-21 16:28:29 -0300

    Impressão com laço for

---
+++ LOGICA/src/javalogica/EX01.java
@@ -0,0 +1,17 @@
+package javalogica;
+
+public class EX01 {
+	
+	public static void main(String[] args) {
+		
+		//Escreva um algoritmo para imprimir os 10 primeiros números inteiros maiores que 100. 
+		
+		int n;
+		
+		for( n = 101; n <= 110; n++) {
+			
+			System.out.println(n);
+		}
+	}
+
+}
